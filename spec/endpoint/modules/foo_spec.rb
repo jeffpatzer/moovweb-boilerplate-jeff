101198eb3ff4cf071b11426d9026bc057b159a78
Joe Nelson <cred+github@begriffs.com>
2012-10-27 00:15:05 -0500

    DRY host name in spec

--- spec/endpoint/modules/foo_spec.rb
+++ spec/endpoint/modules/foo_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "The foo module" do
   %w(bar baz/1).each do |path|
-    full_path = "http://www.example.com/#{path}"
+    full_path = "#{Capybara.app_host}/#{path}"
     context "on path #{full_path}" do
       visit full_path
 
