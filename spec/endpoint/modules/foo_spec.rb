1c5157eca7c40ead814fece8a6d187d565df2dc4
Joe Nelson <cred+github@begriffs.com>
2012-11-26 17:52:39 -0600

    Use proper rspec before filter

--- spec/endpoint/modules/foo_spec.rb
+++ spec/endpoint/modules/foo_spec.rb
@@ -1,10 +1,12 @@
 require 'spec_helper'
 
 describe "The foo module" do
-  %w(bar baz/1).each do |path|
-    full_path = "#{Capybara.app_host}/#{path}"
+  %w(/bar /baz/1).each do |path|
+    full_path = "#{Capybara.app_host}#{path}"
     context "on path #{full_path}" do
-      visit full_path
+      before(:each) do
+        visit full_path
+      end
 
       it "exists" do
         page.should have_selector('div.foo')
